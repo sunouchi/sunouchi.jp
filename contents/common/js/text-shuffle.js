a9e98e2d14e851baa295f881d98febe318584803
sunouchi <t@sunouchi.jp>
2015-05-14 19:24:37 +0900

    TextShuffleのバグ修正

--- contents/common/js/text-shuffle.js
+++ contents/common/js/text-shuffle.js
@@ -33,9 +33,14 @@ TextShuffle.prototype = {
     }
 
     this.timeStart = new Date().getTime();
-    this.intervalId = setInterval(Delegate.create(this.onInterval, this), 1000/this.frameRate);
+    this.intervalId = setInterval(Delegate.create(this.onInterval, this), 10);
+    // this.intervalId = setInterval(Delegate.create(this.onInterval, this), 1000/this.frameRate);
     this.isRunning = true;
     this.element.html(_str);
+
+    // console.log(this.element);
+    // return false;
+    
   },
 
   stop: function() {
@@ -68,7 +73,8 @@ TextShuffle.prototype = {
   },
 
   getRandomLetter: function() {
-    var _chars = '!"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~';
+    var _chars = '*+,-./0123456789abcdefhiklmnorstuvwxz';
+    // var _chars = '!"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~';
     // var _chars = [
     //   '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~'];
 
